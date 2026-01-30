a05490c05f7cbef73aef8ba63e7e6745b4943a1e
Piioni <rangelc.juane@gmail.com>
2026-01-30 06:37:29 +0100

    Removed experience page, fix

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,5 @@
 export { default as Home } from './Home';
 
 // Lazy loaded pages - use React.lazy() in router
-export { default as Experience } from './Experience';
 export { default as Projects } from './Projects';
 export { default as Blog } from './Blog';
